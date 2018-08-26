98ab997b2427851ff2f425d05fa9c9ddf9c6a456
James Townsend <jamestownsend@Jamess-MacBook-Air.local>
2018-08-26 00:07:37 -0600

    version 1

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Mcdonalds');
+INSERT INTO burgers (name) VALUES ('Wendy');
+INSERT INTO burgers (name, eaten) VALUES ('Burger King', true);
+INSERT INTO burgers (name, eaten) VALUES ('In and Out', true);
+INSERT INTO burgers (name, eaten) VALUES ('Smash Burger', true);
+INSERT INTO burgers (name) VALUES ('Five Guys');
